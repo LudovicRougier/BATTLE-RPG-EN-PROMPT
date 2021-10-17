650f76c375e505d0dd8a10880197e3aa1269ef31
LudovicRougier <ludovic.rougier.1993@gmail.com>
2021-10-17 21:36:09 +0200

    Ajout de la fonction de création de personnages

--- JS/main.js
+++ JS/main.js
@@ -52,59 +52,90 @@ function parry(strength){
     );
 }
 
+//Fonction de génération de personnage
+
+function generatingPlayer(name, strength, hp){
+    var character = [];
+
+    hp = generatingRandomValue( strength + 10, strength + 20 );
+
+    character['name'] = name;
+    character['action'] = null;
+    character['previous_action'] = null;
+    character['strength'] = strength;
+    character['hp'] = hp;
+
+    return character;
+}
+
+
 function playRpgGame(){
     var player1 = [];
     var computer = [];
     var turn = null;
     var damage = null;
     var parryValue = 0;
+    var nickname;
+    var strength;
+    var hp;
 
-    player1['name'] = askPlayer1( 'Quel est votre nom ?');
+    const ACTION_ATTACK = 1;
+    const ACTION_DEFEND = 2;
+    const ACTION_PREPARE = 3;
+    const ACTION_RUN = 4;
+
+    nickname = askPlayer1('Quel est votre nom ?');
+    strength = parseInt(askPlayer1('Quelle est votre force'), 10);
+    hp = generatingRandomValue( strength + 10, strength + 20 );
+    
+/*     player1['name'] = askPlayer1( 'Quel est votre nom ?');
     player1['action'] = null;
     player1['previous_action'] = null;
     player1['strength'] = parseInt(askPlayer1('Quelle est votre force'), 10);
-    player1['hp'] = generatingRandomValue( player1['strength'] + 10, player1['strength'] + 20 );
+    player1['hp'] = generatingRandomValue( player1['strength'] + 10, player1['strength'] + 20 ); */
+    player1 = generatingPlayer(nickname, strength, hp);
 
-    computer['name'] = 'Ennemi';
+/*     computer['name'] = 'Ennemi';
     computer['action'] = null;
     computer['previous_action'] = null;
     computer['strength'] = generatingRandomValue( player1['strength'], player1['strength'] + 5 );
-    computer['hp'] = generatingRandomValue( player1['strength'] + 10, player1['strength'] + 20 );
+    computer['hp'] = generatingRandomValue( player1['strength'] + 10, player1['strength'] + 20 ); */
+    computer = generatingPlayer('Ennemi', strength, hp);
 
 
     turn = generatingRandomValue(1, 2);
 
     while( computer['hp'] > 0 && player1['hp'] > 0 ) {
-        while(turn === 1 && (player1['action'] !== 1 || player1['action'] !== 2 || player1['action'] !== 3 || player1['action'] !== 4)){
+        while(turn === 1 && (player1['action'] !== ACTION_ATTACK || player1['action'] !== ACTION_DEFEND || player1['action'] !== ACTION_PREPARE || player1['action'] !== ACTION_RUN)){
             
             alert('C\'est à vous');
             player1['action'] = parseInt(prompt('Que voulez vous faire ? \n 1. Attaquer \n 2. Bloquer \n 3. Se préparer \n 4. Fuir'), 10);
             
 
-            if (player1['action'] === 1){
+            if (player1['action'] === ACTION_ATTACK){
                 damage = hitEnnemy(player1['strength']); 
                 
-                if (computer['action'] === 2){
+                if (computer['action'] === ACTION_DEFEND){
                     damage = damage - parryValue;
                     if( damage < parryValue ){
                         damage = 0;
                     }
                 }
-                if (player1['previous_action'] === 3 ){
+                if (player1['previous_action'] === ACTION_PREPARE ){
                     damage += 3;
                 }
                 computer['hp'] = computer['hp'] - damage;
                 alert('Vous avez fait ' + damage + ' points de dégats');
                 console.log(computer['hp']);
             }
-            else if (player1['action'] === 2){
+            else if (player1['action'] === ACTION_DEFEND){
                 parryValue = parry(player1['strength']);
                 alert ('Vous réduirez les dégats de ' + parryValue + ' si vous êtes attaqués ce tour');
             }
-            else if (player1['action'] === 3){
+            else if (player1['action'] === ACTION_PREPARE){
                 alert ('Si vous attaquez au prochain tour, vous infligerez 3 points de dégats supplémentaire');
             }
-            else if (player1['action'] === 4){
+            else if (player1['action'] === ACTION_RUN){
                 alert('Vous avez fuit \n rechargez la page pour recommencer');
                 return 0;
             }
@@ -125,7 +156,7 @@ function playRpgGame(){
 
         console.log(computer['action']);
 
-        if (computer['action'] === 1 && computer['hp'] > 0){
+        if (computer['action'] === ACTION_ATTACK && computer['hp'] > 0){
             damage = hitEnnemy(computer['strength'] ) 
                 
             if (player1['action'] === 2){
