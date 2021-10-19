2b4d708486a671eb11e0661e0e97ee8266de5cb2
Thomas LEFETZ <thomas@lefetz.net>
2021-10-19 16:21:16 +0200

    WIP classes

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,173 @@
+function Character(name, weapon) {
+	this.actions = [];
+	
+	this.name = name;
+	this.parryValue = 0;
+	
+	if (weapon instanceof Weapon) {
+		this.weapon = weapon;
+		this.currentHp = this.weapon.getHp();
+		this.maxHp = this.currentHp;
+		this.strength = this.weapon.getStrength();
+	}
+}
+
+Character.prototype.getHp = function () {
+	return this.currentHp;
+}
+
+Character.prototype.getName = function () {
+	return this.name;
+}
+
+Character.prototype.addAction = function (action) {
+	this.actions.push(action);
+}
+
+Character.prototype.getLastAction = function () {
+	if (this.actions.length == 0) {
+		return null;
+	}
+	
+	return this.actions[this.actions.length - 1];
+}
+
+Character.prototype.attack = function (enemy) {
+	
+	var strength = this.strength + this.weapon.getStrength();
+	
+    var damage = Math.round(
+        (Math.random()* (strength/3)) +1
+    );
+	
+	var damageMultiplier = this.weapon.getDamageMultiplier()
+	
+	if (damageMultiplier != 0) {
+	  damage = Math.round(damage * damageMultiplier);
+	}	
+	
+	if (enemy.getLastAction() == ACTION_DEFEND) {
+		var parryValue = enemy.getParryValue();
+        damage = damage - parryValue;
+        if( damage < parryValue ){
+            damage = 0;
+        }
+	}
+
+
+    if (this.getLastAction() === ACTION_PREPARE && this.weapon.getBonusDamage()){
+        damage += this.weapon.getBonusDamage();
+    }
+    else{
+        damage += 3;
+    }
+	
+	enemy.takeDamage(damage);
+	
+	return damage;
+}
+
+Character.prototype.prepareParry = function () {
+	
+    if (this.weapon === WEAPON_SWORD_AND_SHIELD){
+        this.parryValue = Math.round(
+            (Math.random()* (this.weapon.strength) + 5) 
+        );
+		
+		return this.parryValue;
+    }
+	
+    this.parryValue = Math.round(
+        (Math.random()* (strength/3))
+    );
+	
+	return this.parryValue;
+}
+
+Character.prototype.takeDamage = function (damage) {
+	this.currentHp = this.currentHp - damage;
+}
+
+Character.prototype.getParryValue = function () {
+	var parryValue = this.parryValue;
+	
+	this.parryValue = 0;
+	
+	return parryValue;
+}
+
+Character.prototype.chooseActionAlone = function () {
+    if (this.getLastAction() !== ACTION_ATTACK ){
+        this.addAction(ACTION_ATTACK);
+    } 
+
+    this.addAction(generatingRandomValue(1, 3));
+}
+
+function Weapon(id, name, strength, hp, damageMultiplier = 0, bonusDamage = 0, parryValue = 0) {
+	this.id = id;
+	this.name = name;
+	this.strength = strength;
+	this.hp = hp;
+	this.damageMultiplier = damageMultiplier;
+	this.bonusDamage = bonusDamage;
+	this.parryValue = parryValue;
+}
+
+Weapon.prototype.getId = function () {
+	return this.id;
+}
+
+Weapon.prototype.getStrength = function () {
+	return this.strength;
+}
+
+Weapon.prototype.getDamageMultiplier = function () {
+	return this.damageMultiplier;
+}
+
+Weapon.prototype.getBonusDamage = function () {
+	return this.bonusDamage;
+}
+
+Weapon.prototype.getHp = function () {
+	return this.hp;
+}
+
+const WEAPON_SWORD_AND_SHIELD = new Weapon(1, 'Sword and shield', 15, generatingRandomValue(35, 55));
+const WEAPON_AXE = new Weapon(2, 'Axe', 20, generatingRandomValue(30, 40));
+const WEAPON_DAGGERS = new Weapon(3, 'Daggers', 12.5, generatingRandomValue(17.5, 22.5), 2, 6);
+const WEAPON_STAFF = new Weapon(4, 'Staff', 9, generatingRandomValue(12, 15), 1.5, 3);
+
+
+
+//Fonction de génération de personnage
+
+function generatingPlayer(name, weaponId){
+	
+	var weapon = null;
+	
+	switch (weaponId) {
+		case WEAPON_SWORD_AND_SHIELD.getId():
+		weapon = WEAPON_SWORD_AND_SHIELD;
+		break;
+		
+		case WEAPON_AXE.getId():
+		weapon = WEAPON_AXE;
+		break;
+		
+		case WEAPON_DAGGERS.getId():
+		weapon = WEAPON_DAGGERS;
+		break;
+		
+		case WEAPON_STAFF.getId():
+		weapon = WEAPON_STAFF;
+		break;
+	}
+	
+    return new Character(name, weapon);
+}
+
 /*  
  Fonctions 
 */
@@ -26,218 +196,83 @@ function generatingRandomValue(min, max) {
         (Math.random()* (min - max )) +max
     );
 }
-/*
-fonction générant un nombre aléatoire dépendant de la strength du joueur
 
-@param int strength
-
-@return int
-*/
-function hitEnemy(strength, ennemyAction, playerPreviousAction, ennemyHp, weapon){
-    var damage = Math.round(
-        (Math.random()* (strength/3)) +1
-    );
-    if (weapon === DAGGERS){
-        damage *= 2;
-    }
-    if (weapon === STAFF) {
-        damage = Math.round(damage*1.5);
-        parryValue = 0;
-    }
-
-    if (ennemyAction === ACTION_DEFEND){
-        damage = damage - parryValue;
-        if( damage < parryValue ){
-            damage = 0;
-        }
-    }
-    if (playerPreviousAction === ACTION_PREPARE && weapon === STAFF ){
-        damage += 6;
-    }
-    else{
-        damage += 3;
-    }
-
-    var newEnnemyHP = ennemyHp - damage;
-
-    return [damage, newEnnemyHP];
+function isValidAction(action) {
+	return action > 0 & action < 5;
 }
 
-/*
-fonction générant un nombre aléatoire dépendant de la strength du joueur
-
-@param int strength
-
-@return int
-*/
-function parry(strength, weapon){
-    if (weapon === SWORD_AND_SHEILD){
-        return Math.round(
-            (Math.random()* (strength) + 5) 
-        );
-    }
-    return Math.round(
-        (Math.random()* (strength/3))
-    );
-}
-
-//Fonction de génération de personnage
-
-function generatingPlayer(name, weapon){
-    var character = {};
-    var characteristic = null;
-
-// Determination des dégats et des pv en fonction de l'arme choisie
-    if (weapon === SWORD_AND_SHEILD){
-        characteristic = 15;
-        hp = generatingRandomValue( characteristic + 20, characteristic + 40 );
-    }
-    else if (weapon === AXE) {
-        characteristic = 20;
-        hp = generatingRandomValue( characteristic + 10, characteristic + 20 );
-    }
-    else if (weapon === DAGGERS) {
-        characteristic = 12.5;
-        hp = generatingRandomValue( characteristic+ 5, characteristic + 10 );
-
-    }
-    else if (weapon === STAFF) {
-        characteristic = 9;
-        hp = generatingRandomValue( characteristic + 3, characteristic + 6 );
-    }
-
-// Création du personnage
-    character.name = name;
-    character.action = null;
-    character.previous_action = null;
-    character.weapon = weapon;
-    character.strength = characteristic;
-    character.hp = hp;
-
-    return character;
-}
-
-function computerDecision(previousAction){
-
-    if (previousAction !== ACTION_ATTACK ){
-
-        return ACTION_ATTACK;
-    } 
-
-    return generatingRandomValue(1, 3);
+function isGameFinish(player, computer) {
+	return computer.getHp() <= 0 || player.getHp() <= 0;
 }
 
 
 function playRpgGame(){
-    var player1 = [];
-    var computer = [];
-    var turn = null;
-    var damage = null;
-    var nickname;
-    var weapon;
-    var hp;
+    var turn = generatingRandomValue(1, 2);
 
-/*     const ACTION_ATTACK = 1;
-    const ACTION_DEFEND = 2;
-    const ACTION_PREPARE = 3;
-    const ACTION_RUN = 4;
- */
-
-    nickname = askPlayer1('Quel est votre nom ?');
-    weapon = parseInt(askPlayer1('Quelle arme voulez vous utiliser ? \n 1. Epée \n 2. Hache \n 3. Dagues \n 4. Bâton'), 10);
+    var nickname = askPlayer1('Quel est votre nom ?');
+    var weapon = parseInt(askPlayer1('Quelle arme voulez vous utiliser ? \n 1. Epée \n 2. Hache \n 3. Dagues \n 4. Bâton'), 10);
     
-    player1 = generatingPlayer(nickname, weapon);
+    var player1 = generatingPlayer(nickname, weapon);
+    var computer = generatingPlayer('Ennemi', weapon);
+	
+	console.log(player1);
+	console.log(computer);
+	console.log(turn);
 
-    computer = generatingPlayer('Ennemi', weapon);
+    while(!isGameFinish(player1, computer)) {
+		var currentCharacter = null;
+		var otherCharacter = null;
+		
+		if (turn == 1) {
+			var newAction = parseInt(prompt('Que voulez vous faire ? \n 1. Attaquer \n 2. Bloquer \n 3. Se préparer \n 4. Fuir'), 10);
+			player1.addAction(newAction);
+			currentCharacter = player1;
+			otherCharacter = computer;
+		}
+		else {
+			computer.chooseActionAlone();
+			currentCharacter = computer;
+			otherCharacter = player1;
+		}
+		
+		console.log(currentCharacter);
+		console.log(otherCharacter);
 
-
-    turn = generatingRandomValue(1, 2);
-
-    while( computer['hp'] > 0 && player1['hp'] > 0 ) {
-        while(turn === 1 && (player1['action'] !== ACTION_ATTACK || player1['action'] !== ACTION_DEFEND || player1['action'] !== ACTION_PREPARE || player1['action'] !== ACTION_RUN)){
-            
-            player1['action'] = parseInt(prompt('Que voulez vous faire ? \n 1. Attaquer \n 2. Bloquer \n 3. Se préparer \n 4. Fuir'), 10);
-            
-
-            if (player1['action'] === ACTION_ATTACK){
-
-
-                 var results = hitEnemy(player1.strength, computer['action'], player1['previous_action'], computer['hp'], player1.weapon); 
-
-                damage = results[0];
-                computer['hp'] = results[1];
-
-                alert('Vous avez fait ' + damage + ' points de dégats');
-            }
-            else if (player1['action'] === ACTION_DEFEND){
-                parryValue = parry(player1.strength, player1.weapon);
-                alert ('Vous réduirez les dégats de ' + parryValue + ' si vous êtes attaqués ce tour');
-            }
-            else if (player1['action'] === ACTION_PREPARE){
-                alert ('Si vous attaquez au prochain tour, vous infligerez 3 points de dégats supplémentaire');
-            }
-            else if (player1['action'] === ACTION_RUN){
-                alert('Vous avez fuit \n rechargez la page pour recommencer');
-                return 0;
-            }
-            else if (player1['hp'] <= 0){
-                alert('Vous avez perdu \n rechargez la page pour recommencer');  
-                return 0;
-            }
-            else {
-                alert('La réponse doit être 1 ou 2 ou 3 ou 4, vous avez perdu un tour');
-            }
-            turn =2;
-            player1['previous_action'] = player1['action'];
-        }
-
-        alert(player1['name'] + ' a ' + player1['hp'] + ' points de vie\n l\'' + computer['name'] + ' a ' + computer['hp'] + ' points de vie' );
-
-        /* * IA de l'adversaire afin qu'il attaque plus souvent au lieu de ne rien faire
-        if (computer.previous_action === ACTION_PREPARE ){
-
-            computer.action = ACTION_ATTACK;
-        } 
-        else if (computer.previous_action === ACTION_DEFEND ) {
-
-            computer.action = ACTION_ATTACK;
-        }
-        else {
-            computer.action = generatingRandomValue(1, 3);
-        } */
-
-        computer.action = computerDecision(computer.previous_action);
-
-        if (computer['action'] === ACTION_ATTACK && computer['hp'] > 0){
-            var results = hitEnemy(computer.strength, player1['action'], computer['previous_action'], player1['hp'], computer.weapon);
-        
-            damage = results[0];
-            player1['hp'] = results[1];
-            alert('Vous avez subit ' + damage + ' points de dégats');
-        }
-        else if (computer['action'] === ACTION_DEFEND  && computer['hp'] > 0){
-            parryValue = parry(computer.strength, computer.weapon);
-            alert ('l\''+ computer['name'] + ' réduira les dégats de ' + parryValue + ' si vous attaquez ce tour');
-        }
-        else if (computer['action'] === ACTION_PREPARE  && computer['hp'] > 0){
-            alert ('Si vous vous faites attaquer au prochain tour, vous subirez 3 points de dégats supplémentaires');
-        }
-        else if (computer['hp'] <= 0){
-            alert('Vous avez gagné \n rechargez la page pour recommencer');
-            return 0;
-        }
-        else {
-            alert('La réponse doit être 1 ou 2 ou 3 ou 4');
-        }
-        
-        turn =1;
-        computer['previous_action'] = computer['action'];
-
-        alert(player1['name'] + ' a ' + player1['hp'] + ' points de vie\n l\'' + computer['name'] + ' a ' + computer['hp'] + ' points de vie' );
+		if (currentCharacter.getLastAction() === ACTION_ATTACK){
+			var damage = currentCharacter.attack(otherCharacter);
+			alert(currentCharacter.getName() + ' a fait ' + damage + ' points de dégats');
+		}
+		else if (currentCharacter.getLastAction() === ACTION_DEFEND){
+			var parryValue = currentCharacter.prepareParry();
+			alert (currentCharacter.getName() + ' se défend, il réduira les dégats de ' + parryValue + ' s\'il est attaqué durant ce tour');
+		}
+		else if (currentCharacter.getLastAction() === ACTION_PREPARE){
+			alert (currentCharacter.getName() + ' se prépare, vous infligerez 3 points de dégats supplémentaire si vous attaquez au prochain tour');
+		}
+		else if (currentCharacter.getLastAction() === ACTION_RUN){
+			alert(currentCharacter.getName() + ' a fui \n rechargez la page pour recommencer');
+			return 0;
+		}
+		else {
+			alert('La réponse doit être 1 ou 2 ou 3 ou 4, vous avez perdu un tour');
+		}
+		
+        alert(player1.getName() + ' a ' + player1.getHp() + ' points de vie\n l\'' + computer.getName() + ' a ' + computer.getHp() + ' points de vie' );
+		
+		if (turn == 1) {
+			turn = 2;
+		}
+		else {
+			turn = 1;
+		}
     }
-    if( player1.hp <= 0) {
-        alert('Vous avez perdu \n rechargez la page pour recommencer'); 
+	
+    if( player1.getHp() <= 0) {
+        alert(player1.getName() + ' a perdu \n rechargez la page pour recommencer'); 
     }
+	else {
+        alert(computer.getName() + ' a perdu \n rechargez la page pour recommencer'); 
+	}
 }
 var parryValue = 0;
 
